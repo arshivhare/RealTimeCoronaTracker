c7debe8a38fd32ec49cb7caa53654e80a3caadb3
arshivhare <arshivhare@gmail.com>
2020-05-09 20:10:40 -0400

    Updates

---
+++ precache-manifest.cf41d033a0ea09489482b53eb97228cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b7b60d12469f59f8fd33abd31e08d8",
+    "url": "/RealTimeCoronaTracker/index.html"
+  },
+  {
+    "revision": "88e10a331fb004875dd7",
+    "url": "/RealTimeCoronaTracker/static/css/main.7930e38b.chunk.css"
+  },
+  {
+    "revision": "fa594fd0f18c32d87b7a",
+    "url": "/RealTimeCoronaTracker/static/js/2.d30b515a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/RealTimeCoronaTracker/static/js/2.d30b515a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e10a331fb004875dd7",
+    "url": "/RealTimeCoronaTracker/static/js/main.8a285a88.chunk.js"
+  },
+  {
+    "revision": "1d82f1f9324373abba2f",
+    "url": "/RealTimeCoronaTracker/static/js/runtime-main.83a59128.js"
+  }
+]);
